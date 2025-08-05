605d1301a105bf89f9867a53b10fc48085dbe1c1
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2025-08-05 17:22:02 +0530

    java-3

--- terraform_main_ec2/install-tools.sh
+++ terraform_main_ec2/install-tools.sh
@@ -74,6 +74,7 @@ sudo yum install -y vault
 
 # Run SonarQube using Docker
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
+docker start sonar
 docker ps
 
 
--- terraform_main_ec2/variables.tf
+++ terraform_main_ec2/variables.tf
@@ -64,7 +64,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-08a6efd148b1f7504" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
@@ -76,7 +76,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "us-east-1"   // Replace with the keypair
+  default     = "us-east-1" // Replace with the keypair
 }
 
 variable "instance_name" {
